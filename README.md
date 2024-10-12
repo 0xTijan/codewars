689c051bcf960a95a48a3d4acac796cfa76ade1f
0xTijan <tijan.nartnik@gmail.com>
2024-10-12 11:51:19 +0200

    added newtons method

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ This repo contains my codewars challanges I do from time to time. They are seper
 
 You can view my Codewars profile [here](https://www.codewars.com/users/0xTijan/).
 
-You can also join Codewars [here](www.codewars.com/r/G8KJzw).
+
+# LeetCode
+I am also active on **LeetCode**. You can view my LeetCode profile [here](https://www.leetcode.com/0xtijan/).
+
+
+# Other coding challenges
+Other coding challenges are in the `/others` folder.
--- others/newtons-method.rs
+++ others/newtons-method.rs
@@ -4,6 +4,28 @@
  * https://en.wikipedia.org/wiki/Newton%27s_method
  */
 
-fn find_root(num: i32) -> i32 {
-  todo!();
+fn find_root(num: i32, tolerance: f32) -> f32 {
+    // check for valid input
+    if num < 0 && tolerance >= 1.0 {
+        panic!("Invalid inputs!")
+    }
+
+    // use this method
+    let mut x: f32 = num as f32;
+
+    loop {
+        let root = 0.5 * (x + ((num as f32) / x));
+        if (root-x).abs() < tolerance {
+            break;
+        }
+        x = root;
+    }
+
+    // return approximate value
+    x
+}
+
+fn main() {
+    let result = find_root(21, 0.00000001);
+    println!("The square root of {} is approximately {}", 21, result);
 }
